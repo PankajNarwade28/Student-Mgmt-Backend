17f7b24e61c115c1a29c97755e1353bc42263fba
PankajNarwade28 <pankajnarwade258@gmail.com>
2026-01-23 12:37:00 +0530

    Used DI instead of manually creating pool

--- src/repositories/health.repository.ts
+++ src/repositories/health.repository.ts
@@ -1,11 +1,13 @@
-import { injectable } from 'inversify';
-import { pool } from "../config/db";
+import { inject, injectable } from 'inversify'; 
+import { Pool } from 'pg';
+import { TYPES } from '../config/types';
 
 @injectable()
 export class HealthRepository {
+  constructor(@inject(TYPES.DbPool) private pool: Pool) {}
   async isDatabaseHealthy(): Promise<boolean> {
     try {
-      await pool.query("SELECT 1"); // trivial query to check connectivity
+      await this.pool.query("SELECT 1"); // trivial query to check connectivity
       return true;
     } catch (error: any) {
       console.error("Database health check error:", error);
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,25 +1,26 @@
 // src/repositories/user.repository.ts
-import { injectable } from 'inversify';
-import { pool } from '../config/db';
+import { inject, injectable } from 'inversify'; 
+import { Pool } from 'pg';
+import { TYPES } from '../config/types';
 
 
 @injectable()  //Decorator: This marks the AuthController so that the Inversify container can manage its lifecycle and dependencies
 export class UserRepository {
+ constructor(@inject(TYPES.DbPool) private pool: Pool) {}
   // Find a user by email (active users only)
   async findByEmail(email: string) {
-    const query = 'SELECT * FROM users WHERE email = $1 AND deleted_at IS NULL';
-    const result = await pool.query(query, [email]);
+    const query = 'SELECT * FROM users WHERE email = $1 AND deleted_at IS NULL'; 
+    const result = await this.pool.query(query, [email]);
     return result.rows[0];
-  }
-
+  }  
   // Create a new user
   async createUser(email: string, passwordHash: string, role: string) {
     const query = `
       INSERT INTO users (email, password, role) 
       VALUES ($1, $2, $3) 
       RETURNING id, email, role, created_at
-    `;
-    const result = await pool.query(query, [email, passwordHash, role]);
+    `; 
+    const result = await this.pool.query(query, [email, passwordHash, role]);
     return result.rows[0];
   }
 }
